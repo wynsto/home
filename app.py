abc4028fb301b145d404cc2697297ce55aaaedbf
wynston <wynston@wynston-win10.localdomain>
2022-10-19 20:25:01 +0800

    admin

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, json
+from flask_admin import Admin
 
 app = Flask(__name__)
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
+# Add administrative views here
 
 @app.route("/")
 def hello_world():
