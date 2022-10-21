36f67ee108a0bbdc3568141c64b34c68b37d2a97
Wynston <houyewei@gmail.com>
2022-10-22 07:52:45 +0800

    update map

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 function App() {
   return (
     <div className="App">
-      <MapContainer center={[51.505, -0.09]} zoom={10} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div className="Map">
+        <MapContainer center={[22.302711, 114.177216]} zoom={10} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[22.302711, 114.177216]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 }
